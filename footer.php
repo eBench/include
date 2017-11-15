cb8ab2bdc8179ed2ee94bdc533db32d00b9fa174
Dmitry <mityalebedev@me.com>
2017-11-15 18:01:10 +0200

    Add links and hide twitter

--- footer.php
+++ footer.php
@@ -17,9 +17,9 @@
 		<div class="site-footer grid fluid">
     <h3>Connect with us</h3> <br>
     <div>
-      <a href="#"><i class="fa fa-twitter fa-2x" aria-hidden="true"></i></a>
-      <a href="#"><i class="fa fa-facebook fa-2x" aria-hidden="true"></i></a>
-      <a href="#"><i class="fa fa-youtube fa-2x" aria-hidden="true"></i></a>
+      <!-- <a href="#" target="_blank"><i class="fa fa-twitter fa-2x" aria-hidden="true"></i></a> -->
+      <a href="https://www.facebook.com/IncludeChoir" target="_blank"><i class="fa fa-facebook fa-2x" aria-hidden="true"></i></a>
+      <a href="https://www.youtube.com/channel/UC9BnzWR37qBT-eEU8U6rC3A" target="_blank"><i class="fa fa-youtube fa-2x" aria-hidden="true"></i></a>
     </div>
   </div>
 		<!-- .site-info -->
