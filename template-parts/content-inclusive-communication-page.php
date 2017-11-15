87da2f7b0d5159588cdf33f45725f216a47011bc
Dmitry <mityalebedev@me.com>
2017-11-15 14:04:14 +0200

    Added inclusive communication page

--- template-parts/content-inclusive-communication-page.php
+++ template-parts/content-inclusive-communication-page.php
@@ -14,26 +14,49 @@
 
 	<header class="entry-header masthead-lrg grid ">
 		<?php the_title( '<h1 class="colored-title">', '</h1>' ); ?>
+		<blockquote><?php echo get_post_meta( get_the_ID(), 'subtitle', true ); ?></blockquote>
 	</header><!-- .entry-header -->
 	<!-- Masthead end -->
 
-		<?php /*
-			the_content();
 
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'include' ),
-				'after'  => '</div>',
-			) );
-			*/
-		?>
+		<!-- Content start -->
+		  <div class="main-content grid">
+		    <div class="row">
+		      <div class="col-4 hide-small">
+		        <img src="<?php echo get_post_meta( get_the_ID(), 'image_1', true ); ?>" alt="">
+		        <img src="<?php echo get_post_meta( get_the_ID(), 'image_2', true ); ?>" alt="">
+		        <img src="<?php echo get_post_meta( get_the_ID(), 'image_3', true ); ?>" alt="">
+		      </div>
 
+		      <div class="col-8 col-small-12">
+						<?php
+							the_content();
+
+							wp_link_pages( array(
+								'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'include' ),
+								'after'  => '</div>',
+							) );
+						?>
+		      </div>
+		    </div>
+		  </div>
+		  <!-- Content end -->
+			<!-- Quote start -->
+		  <div class="fluid bg-light-blue">
+		    <div class="quote grid">
+		      <img src="<?php echo get_post_meta( get_the_ID(), 'quote_image', true ); ?>" class="image-circle" alt="">
+		      <blockquote><?php echo get_post_meta( get_the_ID(), 'quote_text', true ); ?></blockquote>
+		      <span class="color-light-gray"><?php echo get_post_meta( get_the_ID(), 'quote_author', true ); ?></span>
+		    </div>
+		  </div>
+		  <!-- Quote end -->
 
 <!-- Boxouts start -->
 <div class="boxouts grid gutterless fluid-gutterless">
 	<div class="row row-equal">
 		<!-- Boxout start -->
  <?php
-  $args = array('exclude' => '2', 'child_of' => '2');
+  $args = array('exclude' => '2', 'number' => '2');
   $pages = get_pages($args);
 	$index = 0;
   foreach ( $pages as $page ) {
@@ -52,7 +75,7 @@
 			$box .='amber';
 		};
 		$box .= ' bg-pattern-'. $index .'">';
-		$box .=	'<img src="' . get_the_post_thumbnail_url($page->ID, 'full') . '" class="image-circle" alt="">
+		$box .=	'<img src="' . get_template_directory_uri() . '/assets/images/placeholder.jpg" class="image-circle" alt="">
 						<h3> <a href="' . get_page_link( $page->ID ) . '">';
 		if (empty(get_post_meta( $page->ID, 'menu-title', true ))) {
 			$box .= $page->post_title;
